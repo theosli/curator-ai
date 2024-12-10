4f51b7631acfde902fecfe5744e1766b3f68f2c8
SLIMANI Theo <theo.slimani@telecomnancy.eu>
2024-12-10 11:57:36 +0100

    feature unsubscription based on token

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,6 +1,8 @@
 -- creation of the table subscribers
 CREATE TABLE IF NOT EXISTS Subscribers (
     id SERIAL PRIMARY KEY, -- unique id, auto-incremented
-    user_email VARCHAR(255) UNIQUE NOT NULL, -- unique email of the user, mendatory
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- date of subscription, default : nowaday
+    user_email VARCHAR(255) UNIQUE NOT NULL, -- unique email of the user, mandatory
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- date of subscription, default : nowaday
+    unsubscribe_token VARCHAR(255) UNIQUE NOT NULL, -- token generated during subscription, unique
+    is_unsubscribed BOOLEAN DEFAULT false
 );
