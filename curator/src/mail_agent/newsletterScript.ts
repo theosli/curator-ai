d76241c8b50d10aa2cf15e7992306b5bcc0c85e2
Pierre Auguste <pierre.auguste4@etu.univ-lorraine.fr>
2025-02-06 16:24:42 +0100

    [fix] env variable names

--- curator/src/mail_agent/newsletterScript.ts
+++ curator/src/mail_agent/newsletterScript.ts
@@ -4,15 +4,14 @@ import { ServerClient } from 'postmark';
 
 dotenv.config({ path: './../.env' });
 
-const sendMail = true;
+const sendMail = false;
 
 // Calls the curateAndGenerateNewsletter function with right parameters then sends the mail
 // TODO : Add all the dynamic part (newsletter parameters, email params)
 
 async function runNewsletter() {
     const { markdown, html } = await curateAndGenerateNewsletter();
-
-    if (!process.env.POSTMARK_API_KEY || !process.env.DEFAULT_POSTMARK_EMAIL) {
+    if (!process.env.POSTMARK_API_KEY || !process.env.DEFAULT_POSTMARK_MAIL) {
         throw new Error(
             'Make sure to define POSTMARK_SERVER_API_KEY and POSTMARK_DEFAULT_MAIL if you want to send mail'
         );
@@ -27,7 +26,7 @@ async function runNewsletter() {
     // Sending email :
     const client = new ServerClient(process.env.POSTMARK_API_KEY as string);
 
-    const mail = process.env.DEFAULT_POSTMARK_EMAIL as string;
+    const mail = process.env.DEFAULT_POSTMARK_MAIL as string;
 
     client.sendEmail({
         From: mail,
